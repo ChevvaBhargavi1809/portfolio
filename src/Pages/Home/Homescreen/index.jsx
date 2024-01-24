c4b30e9a102ee13765d1cefe496efd061f0984b6
Bhargavi Chevva <bhargavichevva18@gmail.com>
2024-01-24 15:44:27 -0800

    first commit

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,10 +1,5 @@
  import HeroSection from "../HeroSection";
- import MySkills from "../MySkills";
- import AboutMe from "../AboutMe";
  import MyPortfolio from "../MyPortfolio";
- import Testimonial from "../Testimonials";
- import ContactMe from "../ContactMe";
- import Footer from "../Footer";
  import Skills from "../Skills";
  import Work from "../Work";
 import Contact from "../Contact";
